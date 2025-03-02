dd0cd98babfb2b6592dbae11293562c9ff96fd89
Mikael Hultgren <blomma@artsoftheinsane.com>
2025-03-02 21:17:05 +0100

    Scratch

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
 #![allow(clippy::type_complexity)]
 
-mod actions;
-mod audio;
-mod loading;
-mod menu;
-mod player;
-
-use crate::actions::ActionsPlugin;
-use crate::audio::InternalAudioPlugin;
-use crate::loading::LoadingPlugin;
-use crate::menu::MenuPlugin;
-use crate::player::PlayerPlugin;
-
 use bevy::app::App;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
@@ -25,7 +13,7 @@ enum GameState {
     // During the loading State the LoadingPlugin will load our assets
     #[default]
     Loading,
-    // During this State the actual game logic is executed
+    // During this State the actual game ldogic is executed
     Playing,
     // Here the menu is drawn and waiting for player interaction
     Menu,
@@ -35,13 +23,7 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.init_state::<GameState>().add_plugins((
-            LoadingPlugin,
-            MenuPlugin,
-            ActionsPlugin,
-            InternalAudioPlugin,
-            PlayerPlugin,
-        ));
+        app.init_state::<GameState>();
 
         #[cfg(debug_assertions)]
         {
